1eb3b62bc7c5d559cedcdcb595fd4cb6f459c7e8
Amosov Maxim <faystmax@gmail.com>
2020-09-12 17:41:50 +0700

    Add FilterType#MAX_POSITION and refactor SymbolFilter

--- src/main/java/com/faystmax/binance/api/client/domain/SymbolFilter.java
+++ src/main/java/com/faystmax/binance/api/client/domain/SymbolFilter.java
@@ -3,78 +3,84 @@ package com.faystmax.binance.api.client.domain;
 import com.faystmax.binance.api.client.domain.enums.FilterType;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 /**
- * Filters define trading rules on a symbol or an exchange. Filters come in two forms: symbol filters and exchange filters.
- * <p>
- * The PRICE_FILTER defines the price rules for a symbol.
- * <p>
- * The LOT_SIZE filter defines the quantity (aka "lots" in auction terms) rules for a symbol.
- * <p>
- * The MIN_NOTIONAL filter defines the minimum notional value allowed for an order on a symbol. An order's notional value is the price * quantity.
- * <p>
- * The MAX_NUM_ORDERS filter defines the maximum number of orders an account is allowed to have open on a symbol. Note that both "algo" orders and normal orders are counted for this filter.
- * <p>
- * The MAX_ALGO_ORDERS filter defines the maximum number of "algo" orders an account is allowed to have open on a symbol. "Algo" orders are STOP_LOSS, STOP_LOSS_LIMIT, TAKE_PROFIT, and TAKE_PROFIT_LIMIT orders.
+ * Filters define trading rules on a symbol or an exchange. Filters come in two forms: symbol filters and exchange filters
+ *
+ * @see FilterType#PRICE_FILTER defines the price rules for a symbol
+ * @see FilterType#LOT_SIZE filter defines the quantity (aka "lots" in auction terms) rules for a symbol
+ * @see FilterType#MIN_NOTIONAL filter defines the minimum notional value allowed for an order on a symbol. An order's notional value is the price * quantity
+ * @see FilterType#MAX_NUM_ORDERS filter defines the maximum number of orders an account is allowed to have open on a symbol. Note that both "algo" orders and normal orders are counted for this filter
+ * @see FilterType#MAX_ALGO_ORDERS filter defines the maximum number of "algo" orders an account is allowed to have open on a symbol. "Algo" orders are STOP_LOSS, STOP_LOSS_LIMIT, TAKE_PROFIT, and TAKE_PROFIT_LIMIT orders
+ * @see FilterType#MAX_POSITION filter defines the allowed maximum position an account can have on the base asset of a symbol
  */
 @Data
 public class SymbolFilter {
 
     private FilterType filterType;
 
-    // PRICE_FILTER
-
     /**
-     * Defines the minimum price/stopPrice allowed.
+     * Defines the minimum price/stopPrice allowed
+     *
+     * @see FilterType#PRICE_FILTER
      */
-    private String minPrice;
-
+    private BigDecimal minPrice;
     /**
-     * Defines the maximum price/stopPrice allowed.
+     * Defines the maximum price/stopPrice allowed
+     *
+     * @see FilterType#PRICE_FILTER
      */
-    private String maxPrice;
-
+    private BigDecimal maxPrice;
     /**
-     * Defines the intervals that a price/stopPrice can be increased/decreased by.
+     * Defines the intervals that a price/stopPrice can be increased/decreased by
+     *
+     * @see FilterType#PRICE_FILTER
      */
-    private String tickSize;
-
-
-    // LOT_SIZE
+    private BigDecimal tickSize;
 
     /**
      * Defines the minimum quantity/icebergQty allowed.
+     *
+     * @see FilterType#LOT_SIZE
      */
-    private String minQty;
-
+    private BigDecimal minQty;
     /**
      * Defines the maximum quantity/icebergQty allowed.
+     *
+     * @see FilterType#LOT_SIZE
      */
-    private String maxQty;
-
+    private BigDecimal maxQty;
     /**
-     * Defines the intervals that a quantity/icebergQty can be increased/decreased by.
+     * Defines the intervals that a quantity/icebergQty can be increased/decreased by
+     *
+     * @see FilterType#LOT_SIZE
      */
     private String stepSize;
 
-    // MIN_NOTIONAL
-
     /**
-     * Defines the minimum notional value allowed for an order on a symbol. An order's notional value is the price * quantity.
+     * Defines the minimum notional value allowed for an order on a symbol. An order's notional value is the price * quantity
+     *
+     * @see FilterType#MIN_NOTIONAL
      */
     private String minNotional;
 
-
-    // MAX_NUM_ALGO_ORDERS
-
     /**
-     * Defines the maximum number of "algo" orders an account is allowed to have open on a symbol. "Algo" orders are STOP_LOSS, STOP_LOSS_LIMIT, TAKE_PROFIT, and TAKE_PROFIT_LIMIT orders.
+     * Defines the maximum number of "algo" orders an account is allowed to have open on a symbol. "Algo" orders are STOP_LOSS, STOP_LOSS_LIMIT, TAKE_PROFIT, and TAKE_PROFIT_LIMIT orders
+     *
+     * @see FilterType#MAX_NUM_ALGO_ORDERS
      */
     private String maxNumAlgoOrders;
 
     /**
-     * MAX_NUM_ORDERS filter defines the maximum number of orders an account is allowed to have open on a symbol. Note that both "algo" orders and normal orders are counted for this filter.
-     * MAX_ALGO_ORDERS filter defines the maximum number of "algo" orders an account is allowed to have open on a symbol. "Algo" orders are STOP_LOSS, STOP_LOSS_LIMIT, TAKE_PROFIT, and TAKE_PROFIT_LIMIT orders.
-     * ICEBERG_PARTS filter defines the maximum parts an iceberg order can have. The number of ICEBERG_PARTS is defined as CEIL(qty / icebergQty).
+     * @see FilterType#MAX_NUM_ORDERS filter defines the maximum number of orders an account is allowed to have open on a symbol. Note that both "algo" orders and normal orders are counted for this filter
+     * @see FilterType#MAX_ALGO_ORDERS filter defines the maximum number of "algo" orders an account is allowed to have open on a symbol. "Algo" orders are STOP_LOSS, STOP_LOSS_LIMIT, TAKE_PROFIT, and TAKE_PROFIT_LIMIT orders
+     * @see FilterType#ICEBERG_PARTS filter defines the maximum parts an iceberg order can have. The number of ICEBERG_PARTS is defined as CEIL(qty / icebergQty)
      */
     private String limit;
+
+    /**
+     * @see FilterType#MAX_POSITION filter defines the allowed maximum position an account can have on the base asset of a symbol
+     */
+    private BigDecimal maxPosition;
 }
